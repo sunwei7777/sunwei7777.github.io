389b191e61fdad82baf75217200446761d4a5dd0
sunwei7777 <43331155+sunwei7777@users.noreply.github.com>
2025-04-09 16:17:27 +0800

    更新 blogs.js

--- static/blogs.js
+++ static/blogs.js
@@ -8,4 +8,14 @@ export default [{
 	weibo: '',
 	address: '山西省盂县秀水镇西关村9-162',
 	url: ''
+},{
+	type: 'person',
+	id: "140322199304250049",
+	name: "张榕真",
+	phone: "18234062600",
+	qq: '',
+	email: '',
+	weibo: '',
+	address: '山西省盂县秀水镇西关村9-162',
+	url: ''
 }]
