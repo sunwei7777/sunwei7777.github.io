83a58b7502fc54f6da05352585011cd5c972a49d
sunwei7777 <43331155+sunwei7777@users.noreply.github.com>
2025-04-12 22:25:29 +0800

    更新 blogs.js

--- static/blogs.js
+++ static/blogs.js
@@ -10,12 +10,12 @@ export default [{
 	url: ''
 },{
 	type: 'person',
-	id: "140322199304250049",
-	name: "张榕真",
-	phone: "18234062600",
+	id: "370403198612011466",
+	name: "渠静",
+	phone: "13666362280",
 	qq: '',
 	email: '',
 	weibo: '',
-	address: '山西省盂县秀水镇西关村9-162',
+	address: '山东省枣庄市薛城区',
 	url: ''
 }]
